6da9a7c813cb40f40fa8fa0cb085772762977a57
Chiheb Nabil <chiheb.design@gmail.com>
2023-06-26 13:17:18 +0100

    clean code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { App, HTTPReceiver } = require('@slack/bolt');
+const { App } = require('@slack/bolt');
 require('dotenv').config();
 const openai = require('./open_ai.js');
 
@@ -14,102 +14,12 @@ app.error(async (error) => {
 });
 
 
-app.command('/fdbk', async ({ command, ack, say }) => {
-  await ack();
-  await say({
-    blocks: [
-      {
-        "type": "input",
-        "element": {
-          "type": "plain_text_input",
-          "multiline": true,
-          "action_id": "reject_input"
-        },
-        "label": {
-          "type": "plain_text",
-          "text": "Candidate main reject reasons (Bullet points)",
-          "emoji": true
-        }
-      },
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "A personal rejection message to the candidate"
-        },
-        "accessory": {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Generate a reject message",
-            "emoji": true
-          },
-          "value": "click_me_123",
-          "action_id": "reject_button_click"
-        }
-      }
-    ],
-  });
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
 });
 
-
-app.command('/pitch', async ({ command, ack, say }) => {
-  await ack();
-  await say({
-    blocks: [
-      {
-        "type": "input",
-        "element": {
-          "type": "plain_text_input",
-          "multiline": true,
-          "action_id": "pitch_input"
-        },
-        "label": {
-          "type": "plain_text",
-          "text": "Candidate main expertise (Bullet points)",
-          "emoji": true
-        }
-      },
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "A short and concise recommendation"
-        },
-        "accessory": {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Generate a pitch",
-            "emoji": true
-          },
-          "value": "click_me_123",
-          "action_id": "pitch_button_click"
-        }
-      }
-    ],
-  });
-});
-
-app.action('pitch_button_click', async ({ body, ack, say }) => {
-  await ack();
-  let blockId = body.message.blocks[0].block_id
-  let pitch = body.state.values[blockId].pitch_input.value
-  const rs = await openai.openai.createChatCompletion({
-    model: "gpt-3.5-turbo-16k",
-    messages: [
-      {
-        role: "user",
-        content: "Here's the candidate information :\n\n" + pitch
-      },
-      {
-        role: "system",
-        content: openai.CANDIDATE_PITCH_SYSTEM_PROMPT
-      }
-    ],
-  })
-  await say(`<@${body.user.id}> ${rs.data.choices[0].message.content}`);
-});
 
 (async () => {
   await app.start();
